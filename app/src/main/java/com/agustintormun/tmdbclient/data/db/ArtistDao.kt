3c79cb81113d3e0b716d519aa6963d5785d6b4ef
Agustin Torres <agustin.tormun@gmail.com>
2022-12-26 12:32:21 -0600

    Creating db classes and interfaces

---
+++ app/src/main/java/com/agustintormun/tmdbclient/data/db/ArtistDao.kt
@@ -0,0 +1,19 @@
+package com.agustintormun.tmdbclient.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.agustintormun.tmdbclient.data.model.artist.Artist
+
+@Dao
+interface ArtistDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveArtists(artists: List<Artist>)
+
+    @Query("DELETE FROM popular_artist")
+    suspend fun deleteAllArtists()
+
+    @Query("SELECT * FROM popular_artist")
+    suspend fun getArtists(artists: List<Artist>)
+}
--- app/src/main/java/com/agustintormun/tmdbclient/data/model/tv/TVShow.kt
+++ app/src/main/java/com/agustintormun/tmdbclient/data/model/tv/TVShow.kt
@@ -1,11 +1,15 @@
 package com.agustintormun.tmdbclient.data.model.tv
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "popular_tvshows")
 data class TVShow(
     @SerializedName("first_air_date")
     val firstAirDate: String?,
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
     @SerializedName("name")
